159e3df7d31863117aa31db1b133ac19938efe59
Rakibul29h <rakibul2170@gmail.com>
2025-11-10 22:44:25 +0600

    create user collection in database done

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const express=require('express');
-const cors=require('cors');
-require('dotenv').config();
-const app=express();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-const port=process.env.PORT||3000;
+const port = process.env.PORT || 3000;
 const uri = process.env.MONGODB_URI;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -22,16 +21,29 @@ async function run() {
     await client.connect();
     const database = client.db("habitTracker");
     const habitsCollection = database.collection("habits");
+    const userscollection = database.collection("users");
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const email = req.body.email;
+      const query = { email: email };
+      const existingUser = await userscollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists" });
+      } else {
+        const result = await userscollection.insertOne(user);
+        res.send(result);
+      }
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-   
-    await client.close();
   }
 }
 run().catch(console.dir);
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
